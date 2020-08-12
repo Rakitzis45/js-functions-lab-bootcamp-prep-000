6086324a7618d4a418fa2b52c8707cf21a4624a8
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-12 16:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 console.log(happyHolidays());
+
+function happyHolidaysTo(){
+  return `Happy holidays ${name}!`;
+}
+
+function happyCustomHolidayTo(){
+  return `Happy ${holiday}, ${name}!`;
+}
+function holidayCountdown(){
+  return `It's ${days} days until ${holiday}!`;
+}
